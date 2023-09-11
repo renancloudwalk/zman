ae0d40487174d44e1273b45dbd4ef7c1cdbca536
renancloudwalk <renan@cloudwalk.io>
2023-09-11 18:19:39 -0300

    fix: padding

--- content_script.js
+++ content_script.js
@@ -1,21 +1,21 @@
-	var importObject = {
+var importObject = {
   imports: {
-      imported_func: function(arg) {
-        console.log(arg);
-      }
+    imported_func: function(arg) {
+      console.log(arg);
     }
-  };
-  
-  var response = null;
-  var bytes = null;
-  var results = null;
-  
-  
-  var wasmPath = chrome.runtime.getURL("simple.wasm");
-  fetch(wasmPath).then(response =>
-	response.arrayBuffer()
-	).then(bytes =>
-		WebAssembly.instantiate(bytes, importObject)
-		).then(results => {
-		results.instance.exports.exported_func();
+  }
+};
+
+var response = null;
+var bytes = null;
+var results = null;
+
+
+var wasmPath = chrome.runtime.getURL("simple.wasm");
+fetch(wasmPath).then(response =>
+  response.arrayBuffer()
+).then(bytes =>
+  WebAssembly.instantiate(bytes, importObject)
+).then(results => {
+  results.instance.exports.exported_func();
 });
