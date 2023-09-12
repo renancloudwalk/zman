53e95c086410a6c897ab6905a3f9c0e92028d6ef
renancloudwalk <renan@cloudwalk.io>
2023-09-12 16:58:28 -0300

    fix: stop overwriting manifest file

--- build.sh
+++ build.sh
@@ -1,28 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-rm -rf pkg
 wasm-pack build --target=no-modules || exit 1
 
-# Find value of argument --manifest-version
-for arg in "$@"; do
-  case "$arg" in
-    --manifest-version=*)
-      manifest_version="${arg#*=}"
-      ;;
-  esac
-done
-
 if [ ! -f "pkg/manifest.json" ]; then
-  # Copy manifest.json to pkg
-  if [ "$manifest_version" = "v3" ] || [ "$manifest_version" = "3" ]; then
-    cp manifest_v3.json pkg/manifest.json
-  else if [ "$manifest_version" = "v2" ] || [ "$manifest_version" = "2" ]; then
     cp manifest_v2.json pkg/manifest.json
-  else
-    echo "Packaging with manifest version v2. Pass --manifest-version=v3 to package with manifest version 3."
-    cp manifest_v2.json pkg/manifest.json
-    fi
-  fi
 fi
 
 
